02cb885d529a86f956b86340a3b4ea471c24b163
mlaguna3 <marlon.laguna@hugoapp.com>
2022-06-29 21:35:08 -0600

    remove all and first component

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Hola from './components/hola'
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Administracion de productos</h2>
+      <Hola></Hola>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/hola.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+function hola(){
+     return(
+          <div>
+              <img src="https://partners.hugoapp.com/img/logo_hugo.png" alt="Girl in a jacket" width="50" height="50"></img>
+              &nbsp;
+              <img src="https://live.pystatic.com/webassets/AppscoreWeb/monolith/4.0.46/pwa/manifest/pedidosya/icons/icon-384x384-5.png" alt="Girl in a jacket" width="50" height="50"></img>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Precio</th>
+      <th scope="col">Inventario</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Leche</td>
+      <td>30</td>
+      <td>55</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Enlatados</td>
+      <td>20</td>
+      <td>45</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>sardina</td>
+      <td>25</td>
+      <td>1989</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>caramelos</td>
+      <td>25</td>
+      <td>1989</td>
+    </tr>
+  </tbody>
+</table>
+          </div>
+     )
+}
+
+export default hola
